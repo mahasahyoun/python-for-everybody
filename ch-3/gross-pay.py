65a5698e5e87588e6912d21d36a3a30d664b5332
mahasahyoun <maha.z.sahyoun@gmail.com>
2020-08-04 21:06:03 +0300

    Update comments

--- ch-3/gross-pay.py
+++ ch-3/gross-pay.py
@@ -1,3 +1,12 @@
+"""
+Write a program to prompt the user for hours and rate per hour using input to
+compute gross pay. Pay the hourly rate for the hours up to 40 and 1.5 times the
+hourly rate for all hours worked above 40 hours. Use 45 hours and a rate of 10.50
+per hour to test the program (the pay should be 498.75). You should use input
+to read a string and float() to convert the string to a number. Do not worry about
+error checking the user input - assume the user types numbers properly.
+"""
+
 hrs = input("Enter Hours:")
 h = float(hrs)
 
@@ -5,6 +14,7 @@ rate = input("Enter Rate:")
 r = float(rate)
 
 # Regular hours
+# Compute regular pay for all worked hours
 regularPay = r * h
 
 # Overtime hours
@@ -13,8 +23,8 @@ if (h > 40):
     overtimePay = extra * 0.5 * r
 else:
 	overtimePay = 0
-    
-# Gross pay
+
+# Compute gross pay
 grossPay = regularPay + overtimePay
 
 print(grossPay)
